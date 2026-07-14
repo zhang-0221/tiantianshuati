1224ed4a4f6bef4ae8457714d93c1d98eaa55a2c
zhang-0221 <2441472151@qq.com>
2026-07-14 23:45:00 +0800

    test: tighten public auth config boundary

--- tests/account-auth-contract.test.mjs
+++ tests/account-auth-contract.test.mjs
@@ -5,6 +5,6 @@ const html = fs.readFileSync(new URL('../index.html', import.meta.url), 'utf8');
 
 assert.match(html, /const LS_KEY = 'ttsk_ds_key'/);
 assert.match(html, /const AUTH_CONFIG = window\.TTSK_AUTH_CONFIG/);
-assert.doesNotMatch(html, /SUPABASE_SERVICE_ROLE_KEY/);
+assert.doesNotMatch(html, /service[_-]?role|serviceRole|SUPABASE_SERVICE_ROLE_KEY/i);
 
 console.log('account auth client contract passed');
